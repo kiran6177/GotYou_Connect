33f9a9ec7169611f7f441f99f3506f5182c9c4e2
kiran6177 <147698281+kiran6177@users.noreply.github.com>
2024-11-04 22:30:43 +0530

    cookie-fix5

--- frontend/src/Config/config.js
+++ frontend/src/Config/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://got-you-connect.vercel.app/api/';
+export const BASE_URL = '/api/';
 export const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 export const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 export const mobileRegex = /^[6-9]\d{9}$/;
